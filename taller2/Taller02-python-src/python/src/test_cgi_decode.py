db60522260e8b4d002ab4d14254abfed8e44e0a5
Agustina Aldasoro <agusaldasoro@gmail.com>
2016-08-17 17:07:54 -0300

    taller 2

---
+++ taller2/Taller02-python-src/python/src/test_cgi_decode.py
@@ -0,0 +1,95 @@
+'''
+Created on Aug 10, 2016
+
+@author: galeotti
+'''
+import unittest
+import cgi_decode
+import coverage 
+import ast
+import sys
+
+class TestCGIDecode(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        #cls.c = coverage.LineCoverage("cgi_decode")	
+        sys.settrace(cls.c.traceit)
+        #cls.c.total_lines = ej3_line_counter()
+        #cls.c = coverage.BranchCoverage("cgi_decode")
+        cls.c.branch_lines = ej4_branch_counter()
+        sys.settrace(cls.c.traceit)
+        cls.c.setUp()
+
+    def test0(self):
+        pass
+    
+    def test1(self):
+        decoded_str = cgi_decode.cgi_decode("test")
+        self.assertEquals(decoded_str, "test")
+ 
+    @unittest.expectedFailure
+    def test2(self):
+        cgi_decode.cgi_decode("+%0d+%4j")
+ 
+    def test3(self):
+        decoded_str = cgi_decode.cgi_decode("abc")
+        self.assertEquals(decoded_str,"abc")
+    
+    @classmethod
+    def tearDownClass(cls): 
+        cls.c.tearDown()
+        cls.c.printCoverage()
+
+class LineCounter(ast.NodeVisitor):
+    def __init__(self):
+        self.lines = set()
+
+    def generic_visit(self, node):
+        if hasattr(node, "lineno"):
+            self.lines.add(node.lineno)
+        ast.NodeVisitor.generic_visit(self, node)
+
+class BranchCounter(ast.NodeVisitor):
+    def __init__(self):
+        self.branches = 0
+        self.branches_lines = set()
+
+    def visit_If(self, node):
+        self.branches += 2
+        self.branches_lines.add(node.body[0].lineno)
+        self.branches_lines.add(node.orelse[0].lineno)
+        self.generic_visit(node)
+
+    def visit_While(self, node):
+        self.branches += 2
+        self.branches_lines.add(node.body[0].lineno)
+        self.generic_visit(node)
+
+# ejercicios
+
+def ej1_run_tests():
+    if __name__ == "__main__":
+        unittest.main()
+
+def ej2_parse_ast():
+    root = ast.parse(open("cgi_decode.py").read())
+    print ast.dump(root)
+
+def ej3_line_counter():
+    root = ast.parse(open("cgi_decode.py").read())
+    v = LineCounter()
+    v.visit(root)
+    return len(v.lines)
+
+def ej4_branch_counter():
+    root = ast.parse(open("cgi_decode.py").read())
+    v = BranchCounter()
+    v.visit(root)
+    return v.branches
+
+#print(ej4_branch_counter())
+#print(ej3_line_counter())
+#ej2_parse_ast()
+ej1_run_tests()
+    
